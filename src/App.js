792f53e2a75f8fe6be91983ed655c1d503dca21e
gnoguer <gerard.noguer@gmail.com>
2022-03-31 22:49:48 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Login from "./components/Login";
+
+function App() {
+    return (
+        <div className="bg-neutral-50">
+             <Login/>
+             
+        </div>
+    );
+}
+
+export default App;
